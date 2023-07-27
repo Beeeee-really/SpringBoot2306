e362e89aa9bb5cfa8c665e38520fe4268c600cee
Beeeee <beeeee2@outlook.com>
2023-07-27 15:46:43 +0800

    Initial commit

---
+++ src/main/java/com/tedu/springboot2306/SpringBoot2306Application.java
@@ -0,0 +1,13 @@
+package com.tedu.springboot2306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2306Application.class, args);
+    }
+
+}
