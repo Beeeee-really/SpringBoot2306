a2c3d13b72f89c1e6da4f675691187e0e61d403b
Beeeee <beeeee2@outlook.com>
2023-07-28 11:25:36 +0800

    .

---
+++ src/main/java/com/tedu/springboot2306/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.tedu.springboot2306.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 当前类用于处理用户数据相关的业务
+ */
+public class UserController {
+
+    public void signUp(HttpServletRequest request, HttpServletResponse response) {
+        
+    }
+
+}
