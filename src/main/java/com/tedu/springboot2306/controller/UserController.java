1c16cfa8b6abc8308e8e94837b2ac316434ba011
Beeeee <beeeee2@outlook.com>
2023-07-28 11:38:35 +0800

    .

--- src/main/java/com/tedu/springboot2306/controller/UserController.java
+++ src/main/java/com/tedu/springboot2306/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.tedu.springboot2306.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,10 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * 当前类用于处理用户数据相关的业务
  */
+//类上要使用@Controller注解标注，否则不能被调用
+@Controller
 public class UserController {
 
+    @RequestMapping("/signupUser")
     public void signUp(HttpServletRequest request, HttpServletResponse response) {
-        
+        System.out.println("处理...");
     }
 
 }
